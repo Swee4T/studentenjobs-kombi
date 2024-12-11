585b2a0d8908cedbe40e1eee911f751a8368cbfb
Oskar MBA1 <orakse@gmail.com>
2024-12-11 12:41:42 +0100

    fix: remove unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useState } from 'react';
+import React from 'react';
 import { Card, CardHeader, CardTitle, CardContent } from './components/ui/card';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from './components/ui/tabs';
 import { 
